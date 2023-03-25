cb546f7e1b27cc5c24152b0c20ce3b3686b28bff
Luis <appelhanzluis@gmail.com>
2023-03-24 23:44:27 -0300

    eliminar anclaje smooth jquery

--- js/animations.js
+++ js/animations.js
@@ -1,55 +1,55 @@
 AOS.init();
 
 /*Magic Animations Library*/
-function containerMain(){
-    let containerPrincipal = document.querySelector('.page');
-    containerPrincipal.classList.add('magictime', 'vanishIn');
+function containerMain() {
+  let containerPrincipal = document.querySelector('.page');
+  containerPrincipal.classList.add('magictime', 'vanishIn');
 }
 setTimeout(containerMain, 1000);
 
-function tituloPrincipal(){
-    let tituloNav = document.querySelector('.navbar-brand');
-    tituloNav.classList.add('magictime', 'vanishIn');
+function tituloPrincipal() {
+  let tituloNav = document.querySelector('.navbar-brand');
+  tituloNav.classList.add('magictime', 'vanishIn');
 }
 setTimeout(tituloPrincipal, 2000);
 
-function listaNav(){
-    let listNav = document.querySelector('.navbar-nav');
-    let iconGit = document.querySelector('.icon-github');
-    let iconLinked = document.querySelector('.icon-linkedin');
-    listNav.style.display = 'flex';
-    iconGit.style.display = 'flex';
-    iconLinked.style.display = 'flex';
-    listNav.classList.add('magictime', 'vanishIn');
+function listaNav() {
+  let listNav = document.querySelector('.navbar-nav');
+  let iconGit = document.querySelector('.icon-github');
+  let iconLinked = document.querySelector('.icon-linkedin');
+  listNav.style.display = 'flex';
+  iconGit.style.display = 'flex';
+  iconLinked.style.display = 'flex';
+  listNav.classList.add('magictime', 'vanishIn');
 }
 setInterval(listaNav, 2000);
 
-function fotoPrincipal(){
-    let fotoInicio = document.querySelector('.img-fluid');
-    fotoInicio.classList.add('magictime', 'vanishIn');
+function fotoPrincipal() {
+  let fotoInicio = document.querySelector('.img-fluid');
+  fotoInicio.classList.add('magictime', 'vanishIn');
 }
 setTimeout(fotoPrincipal, 3000);
 
 function textoPrincipal() {
-    let textoInicio = document.querySelector('.texto-inicial');
-    textoInicio.classList.add('magictime', 'vanishIn');
+  let textoInicio = document.querySelector('.texto-inicial');
+  textoInicio.classList.add('magictime', 'vanishIn');
 }
 setTimeout(textoPrincipal, 4000);
 
 
 function cambiarColorNav() {
-    let navigationBar = document.querySelector(".gradient");
+  let navigationBar = document.querySelector(".gradient");
   if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-    navigationBar.classList.add("gradient-onchange"); 
-  }else if(document.body.scrollTop < 400 || document.documentElement.scrollTop < 400){
+    navigationBar.classList.add("gradient-onchange");
+  } else if (document.body.scrollTop < 400 || document.documentElement.scrollTop < 400) {
     navigationBar.classList.remove("gradient-onchange");
   }
 }
 
 function topFunction() {
-    document.documentElement.scrollTop = 0;
-  }
-  
-window.onscroll = function() {
-    cambiarColorNav();
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function () {
+  cambiarColorNav();
 };
